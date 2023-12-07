cb8b4d25d62b837b01ebc2aa857dc51d272ead44
matudinglin <45813154+matudinglin@users.noreply.github.com>
2023-12-06 23:18:30 -0800

    Test shot boundary

--- Code/test.py
+++ Code/test.py
@@ -1,19 +1,22 @@
-from scenedetect import detect, ContentDetector
+from scenedetect import detect, AdaptiveDetector
 
-scene_list = detect('./Data/Videos/video11.mp4', ContentDetector())
-for i, scene in enumerate(scene_list):
-    print('    Scene %2d: Start %s / Frame %d, End %s / Frame %d' % (
-        i+1,
-        scene[0].get_timecode(), scene[0].get_frames(),
-        scene[1].get_timecode(), scene[1].get_frames(),))
-    
-scene_list = detect('./Data/Queries/video11_1.mp4', ContentDetector())
-for i, scene in enumerate(scene_list):
-    print('    Scene %2d: Start %s / Frame %d, End %s / Frame %d' % (
-        i+1,
-        scene[0].get_timecode(), scene[0].get_frames(),
-        scene[1].get_timecode(), scene[1].get_frames(),))
-    
-#  Scene 124: Start 00:04:35.500 / Frame 8265, End 00:04:49.433 / Frame 8683
-#  Scene  2: Start 00:00:04.467 / Frame 134, End 00:00:18.400 / Frame 552
+# Get all the query videos paths
+query_videos = []
+query_video_path = './Data/Queries/'
+for i in range(1, 12):
+    query_videos.append(query_video_path + f'video{i}_1.mp4')
 
+# Get all the dataset videos paths
+dataset_videos = []
+dataset_video_path = './Data/Videos/'
+for i in range(1, 21):
+    dataset_videos.append(dataset_video_path + f'video{i}.mp4')
+
+for query_video in query_videos:
+    query_scene_list = detect(query_video, AdaptiveDetector(min_scene_len=15, luma_only=True, min_content_val = 1))
+    print(f'============== For query video {query_video.split("/")[-1]} ==============')
+    for i, scene in enumerate(query_scene_list):
+        print('    Scene %2d: Start %s / Frame %d, End %s / Frame %d' % (
+            i+1,
+            scene[0].get_timecode(), scene[0].get_frames(),
+            scene[1].get_timecode(), scene[1].get_frames(),))
