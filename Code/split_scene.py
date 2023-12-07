c0c6926228d0994bed9fd8f4e5e290b31de0245f
matudinglin <45813154+matudinglin@users.noreply.github.com>
2023-12-06 22:55:01 -0800

    Video search using shot boundary

--- Code/split_scene.py
+++ Code/split_scene.py
@@ -1,29 +1,57 @@
 from scenedetect import detect, ContentDetector
 import pickle
+import cv2
 
-video_root = './Data/Queries/'
-NUM_VIDEOS = 11
+video_root = './Data/Videos/'
+NUM_VIDEOS = 20
+
+def compute_motion_vector(cap, frame_old, frame_new):
+    # Read the frames
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_old)
+    ret, frame_old = cap.read()
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_new)
+    ret, frame_new = cap.read()
+    
+    # Convert to grayscale
+    gray_old = cv2.cvtColor(frame_old, cv2.COLOR_BGR2GRAY)
+    gray_new = cv2.cvtColor(frame_new, cv2.COLOR_BGR2GRAY)
+
+    # Detect features to track
+    points1 = cv2.goodFeaturesToTrack(gray_old, maxCorners=100, qualityLevel=0.3, minDistance=7)
+
+    # Compute optical flow (Lucas-Kanade)
+    # Catch the error when there is no feature to track
+    try:
+        points2, st, err = cv2.calcOpticalFlowPyrLK(gray_old, gray_new, points1, None)
+        # Calculate motion vectors
+        motion_vector = points2 - points1
+    except:
+        motion_vector = None
+
+    # Calculate the average motion vector
+    return motion_vector
 
 # Initial the dictionary
 split_scenes = {}
 video_paths = []
 for i in range(1, NUM_VIDEOS + 1):
-    print(f"Processing video {i}...")
-    split_scenes[f'video{i}_1.mp4'] = []
-    video_paths.append(video_root + f'video{i}_1.mp4')
+    split_scenes[f'video{i}.mp4'] = []
+    video_paths.append(video_root + f'video{i}.mp4')
 
 # Get the scenes from the videos
-for video_path in video_paths:
-    scene_list = detect(video_path, ContentDetector())
-    for scene in scene_list:
-        split_scenes[video_path.split('/')[-1]].append((scene[0].get_frames(), scene[1].get_frames()))
+for i, video_path in enumerate(video_paths):
+    print(f"Processing video{i+1}...")
+    scene_list = detect(video_path, ContentDetector(min_scene_len=15))
+    cap = cv2.VideoCapture(video_path)
+    # For all scene except the last one
+    for scene in scene_list[:-1]:
+        frame_old, frame_new = scene[1].get_frames()-1, scene[1].get_frames()
+        motion_vector = compute_motion_vector(cap, frame_old, frame_new)
+        if motion_vector is not None:
+            split_scenes[video_path.split('/')[-1]].append(motion_vector)
 
 # Save the scenes to binary files
 with open('./Data/split_scenes.pkl', 'wb') as f:
     pickle.dump(split_scenes, f)
 
-# Load the scenes from binary files
-with open('./Data/split_scenes.pkl', 'rb') as f:
-    split_scenes = pickle.load(f)
 
-print(split_scenes)
---
+++ Code/video_query.py
@@ -0,0 +1,71 @@
+import pickle
+from scenedetect import detect, ContentDetector
+import cv2
+
+def compute_motion_vector(cap, frame_old, frame_new):
+    # Read the frames
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_old)
+    ret, frame_old = cap.read()
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_new)
+    ret, frame_new = cap.read()
+    
+    # Convert to grayscale
+    gray_old = cv2.cvtColor(frame_old, cv2.COLOR_BGR2GRAY)
+    gray_new = cv2.cvtColor(frame_new, cv2.COLOR_BGR2GRAY)
+
+    # Detect features to track
+    points1 = cv2.goodFeaturesToTrack(gray_old, maxCorners=100, qualityLevel=0.3, minDistance=7)
+
+    # Compute optical flow (Lucas-Kanade)
+    points2, st, err = cv2.calcOpticalFlowPyrLK(gray_old, gray_new, points1, None)
+
+    # Calculate motion vectors
+    motion_vector = points2 - points1
+
+    # Calculate the average motion vector
+    return motion_vector
+
+# Load the scenes from binary files
+with open('./Data/split_scenes.pkl', 'rb') as f:
+    split_scenes = pickle.load(f)
+
+# # Get the scenes from the query video
+# query_video = './Data/Queries/video_1.mp4'
+# query_scene_list = detect(query_video, ContentDetector(min_scene_len=15))
+
+# # Get the frames of the scenes from the query video
+# frame_old = query_scene_list[0][1].get_frames() - 1
+# frame_new = query_scene_list[0][1].get_frames()
+
+# # Compute the motion vector
+# cap = cv2.VideoCapture(query_video)
+# motion_vector = compute_motion_vector(cap, frame_old, frame_new)
+
+# # Compare motion vector with other motion vectors
+# for video, scenes in split_scenes.items():
+#     for scene in scenes:
+#         # Compare the motion vector with the motion vector of the scene
+#         if motion_vector.shape == scene.shape:
+#             diff = motion_vector - scene
+#             # If the difference is small enough, print the video name
+#             if (diff < 0.1).all():
+#                 print(video)
+
+# Compare for all query videos
+query_video_path = './Data/Queries/video{}_1.mp4'
+for i in range(1, 12):
+    query_video = query_video_path.format(i)
+    query_scene_list = detect(query_video, ContentDetector(min_scene_len=15))
+    if len(query_scene_list) == 0:
+        print(f'Query video{i}_1 has no scene detected')
+        continue
+    frame_old = query_scene_list[0][1].get_frames() - 1
+    frame_new = query_scene_list[0][1].get_frames()
+    cap = cv2.VideoCapture(query_video)
+    motion_vector = compute_motion_vector(cap, frame_old, frame_new)
+    for video, scenes in split_scenes.items():
+        for scene in scenes:
+            if motion_vector.shape == scene.shape:
+                diff = motion_vector - scene
+                if (diff < 0.1).all():
+                    print(f'Query video{i}_1 is from {video}')
